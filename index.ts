3e890cc97d4d3cae416160baa2ddaf31450e6a35
sawhney17 <80150109+sawhney17@users.noreply.github.com>
2022-07-17 17:40:32 +0400

    allow in single block #10

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import "@logseq/libs";
-import { BlockCommandCallback } from "@logseq/libs/dist/LSPlugin.user";
+import {
+  BlockCommandCallback,
+  SettingSchemaDesc,
+} from "@logseq/libs/dist/LSPlugin.user";
 import Mercury from "@postlight/mercury-parser";
 import { NodeHtmlMarkdown, NodeHtmlMarkdownOptions } from "node-html-markdown";
 import { splitBlock } from "./splitblock";
@@ -18,24 +21,53 @@ import { splitBlock } from "./splitblock";
 const regexRules =
   /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
 
-const parseBlock = async (e) => {
+const parseBlock = async (e, isSplit) => {
   const block = await logseq.Editor.getBlock(e.uuid);
-  block.content.match(regexRules).forEach((url) => {
+  block?.content?.match(regexRules)?.forEach((url) => {
     console.log(url);
     Mercury.parse(url, { contentType: "html" }).then((result) => {
-      logseq.Editor.insertBatchBlock(
-        e.uuid,
-        splitBlock(NodeHtmlMarkdown.translate(result.content)),
-        {sibling: false}
-      );
+      if (isSplit) {
+        logseq.Editor.insertBatchBlock(
+          e.uuid,
+          splitBlock(NodeHtmlMarkdown.translate(result.content)),
+          { sibling: false }
+        );
+      } else {
+        logseq.Editor.insertBlock(
+          e.uuid,
+          NodeHtmlMarkdown.translate(result.content),
+          { sibling: false }
+        );
+      }
     });
   });
-}
+};
 
 const main = async () => {
   console.log("plugin loaded");
-  logseq.Editor.registerBlockContextMenuItem("Parse URL", parseBlock), 
-  logseq.Editor.registerSlashCommand("Parse URL", parseBlock);
+  // top.addEventListener('message',(ev)=>{console.log("ev")})
+  top?.addEventListener("message", (ev) => {
+    console.log("console.log event added");
+  });
+
+  logseq.Editor.registerBlockContextMenuItem("Parse URL", async (e) => {
+    parseBlock(e, true);
+  }),
+    logseq.Editor.registerBlockContextMenuItem(
+      "Parse URL into single block",
+      async (e) => {
+        parseBlock(e, false);
+      }
+    ),
+    logseq.Editor.registerSlashCommand(
+      "Parse URL into Single Block",
+      async (e) => {
+        parseBlock(e, false);
+      }
+    );
+  logseq.Editor.registerSlashCommand("Parse URL", async (e) => {
+    parseBlock(e, true);
+  });
 };
 
 logseq.ready(main).catch(console.error);
